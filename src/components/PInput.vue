fbf3d30127a08cbd3418df87829318a236806679
liquorburn <droidseven@libero.it>
2019-10-02 17:19:01 +0200

    added input components (text, radio, checkbox)

---
+++ src/components/PInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <input v-on="$listeners"
+        :disabled="disabled"
+        :class="classNames"
+        :type="type"
+        :value="value"
+        >
+</template>
+
+<script>
+export default {
+  name: 'PInput',
+
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+
+    value: {
+      type: String,
+      default: ''
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false,
+      required: false
+    },
+
+    checked: {
+      type: Boolean,
+      default: false,
+      required: false
+    },
+
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    classNames() {
+      return [
+        'puto-input',
+        (this.disabled && '-disabled'),
+        (this.loading && '-loading')
+      ]
+    }
+  }
+}
+</script>
