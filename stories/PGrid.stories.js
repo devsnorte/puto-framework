a03341459048be8f2634519c1231229b3b46befa
oskar-kupski-elpassion <oskar.kupski@elpassion.pl>
2019-10-09 16:14:29 +0200

    storybook indent changed

--- stories/PGrid.stories.js
+++ stories/PGrid.stories.js
@@ -11,28 +11,28 @@ export const basicUsage = () =>
   Render({
     components: { PContainer, PRow, PCol },
     template: `<PContainer>
-                 <PRow>
-                   <PCol :size-sm="10" :size-lg="12">COLUMN</PCol>
-                   <PCol :size-md="10" :size-xl="12">COLUMN</PCol>
-                   <PCol :size-xs="10" :size-lg="12">COLUMN</PCol>
-                 </PRow>
-                 <PRow>
-                   <PCol :size-xs="10" :size-lg="12">COLUMN</PCol>
-                 </PRow>
-               </PContainer>`
+       <PRow>
+         <PCol :size-sm="10" :size-lg="12">COLUMN</PCol>
+         <PCol :size-md="10" :size-xl="12">COLUMN</PCol>
+         <PCol :size-xs="10" :size-lg="12">COLUMN</PCol>
+       </PRow>
+       <PRow>
+         <PCol :size-xs="10" :size-lg="12">COLUMN</PCol>
+       </PRow>
+     </PContainer>`
   })
 
 export const fluidContainer = () =>
   Render({
     components: { PContainer, PRow, PCol },
     template: `<PContainer fluid>
-                 <PRow>
-                   <PCol :size-sm="10" :size-lg="12">COLUMN</PCol>
-                   <PCol :size-sm="10" :size-xl="12">COLUMN</PCol>
-                   <PCol :size-md="10" :size-lg="12">COLUMN</PCol>
-                 </PRow>
-                 <PRow>
-                   <PCol :size-md="10" :size-lg="12">COLUMN</PCol>
-                 </PRow>
-                </PContainer>`
+     <PRow>
+       <PCol :size-sm="10" :size-lg="12">COLUMN</PCol>
+       <PCol :size-sm="10" :size-xl="12">COLUMN</PCol>
+       <PCol :size-md="10" :size-lg="12">COLUMN</PCol>
+     </PRow>
+     <PRow>
+       <PCol :size-md="10" :size-lg="12">COLUMN</PCol>
+     </PRow>
+    </PContainer>`
   })
