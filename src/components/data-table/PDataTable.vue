3a769a26110429d26d720994011eb9a4c20b2bbf
Romulo G. Rodrigues <yemolai@gmail.com>
2019-10-16 09:21:34 -0300

    components usage to PascalCase

--- src/components/data-table/PDataTable.vue
+++ src/components/data-table/PDataTable.vue
@@ -1,25 +1,25 @@
 <template>
   <table :class="classNames">
-    <p-data-table-row v-if="!noHeaders">
-      <p-data-table-header
+    <PDataTableRow v-if="!noHeaders">
+      <PDataTableHeader
         v-for="(header, k) in headers"
         :key="`p-td-${k}`"
         @click="$emit('header-click', header)">
         {{ header.label }}
-      </p-data-table-header>
-    </p-data-table-row>
-    <p-data-table-row
+      </PDataTableHeader>
+    </PDataTableRow>
+    <PDataTableRow
       v-for="(item, k) in items"
       :key="`p-tr-${k}`"
       :striped="striped && k % 2 < 1"
       @click="$emit('row-click', item)">
-      <p-data-table-cell
+      <PDataTableCell
         v-for="(header, k) in headers"
         :key="`p-tr-${k}-td-${header.name}`"
         @click="$emit('cell-click', { item, column: header.name })">
         {{ format(header, item) }}
-      </p-data-table-cell>
-    </p-data-table-row>
+      </PDataTableCell>
+    </PDataTableRow>
   </table>
 </template>
 
