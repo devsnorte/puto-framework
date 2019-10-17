6b412b5d1370fed9e01c2c466451cce31b1d51b1
Romulo G. Rodrigues <yemolai@gmail.com>
2019-10-16 23:08:53 -0300

    implementing pagination control

---
+++ src/components/data-table/PDataTablePagination.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="puto-data-table-pagination">
+    <PButton
+      :class="prevClassNames"
+      :disabled="!hasPrev"
+      @click="$emit('prev')">
+      {{ prevIcon }}
+    </PButton>
+    <PButton
+      :class="nextClassNames"
+      :disabled="!hasNext"
+      @click="$emit('next')"
+    >
+      {{ nextIcon }}
+    </PButton>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PDataTablePagination',
+  computed: {
+    prevClassNames () {
+      return [
+        'puto-data-table-navigation-prev-button',
+        !this.hasPrev && '-disabled'
+      ]
+    },
+    nextClassNames () {
+      return [
+        'puto-data-table-navigation-next-button',
+        !this.hasNext && '-disabled'
+      ]
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
