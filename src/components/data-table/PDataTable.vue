f336e9fb4a7bb97703587f6e5c883631a4448fc2
Romulo G. Rodrigues <yemolai@gmail.com>
2019-10-16 22:32:28 -0300

    implementing sort

--- src/components/data-table/PDataTable.vue
+++ src/components/data-table/PDataTable.vue
@@ -4,12 +4,13 @@
       <PDataTableHeader
         v-for="(header, k) in headers"
         :key="`p-td-${k}`"
-        @click="$emit('header-click', header)">
+        :sort="isSorting(header)"
+        @click="toggleSort(header.name)">
         {{ header.label }}
       </PDataTableHeader>
     </PDataTableRow>
     <PDataTableRow
-      v-for="(item, k) in items"
+      v-for="(item, k) in orderedItems"
       :key="`p-tr-${k}`"
       :striped="striped && k % 2 < 1"
       @click="$emit('row-click', item)">
@@ -47,6 +48,12 @@ export default {
     striped: Boolean,
     noHeaders: Boolean
   },
+  data () {
+    return {
+      sortDirection: '',
+      sortingBy: null
+    }
+  },
   computed: {
     classNames () {
       return [
@@ -54,15 +61,60 @@ export default {
         !this.items.length && '-empty',
         this.loading && '-loading',
       ]
+    },
+    orderedItems () {
+      const { sortingBy: column, sortDirection: direction } = this
+      if (direction !== '' && column) {
+        const desc = direction === 'desc'
+        return this.sortBy(this.items, column, desc)
+      }
+      return this.items
     }
   },
   methods: {
+    isSorting (header) {
+      if (this.sortingBy === header.name) {
+        return this.sortDirection
+      }
+    },
     format (header, item) {
       const value = item[header.name]
       if (header.format && typeof header.format === 'function') {
         return header.format(value)
       }
       return value
+    },
+    createSortBy (columnName, desc = false) {
+      return (a, b) => {
+        const columnA = a[columnName]
+        const columnB = b[columnName]
+        if (columnA > columnB) {
+          return desc ? -1 : 1
+        } else if (columnA < columnB) {
+          return desc ? 1 : -1
+        }
+        return 0
+      }
+    },
+    sortBy(arr, columnName, desc = false) {
+      const byColumnName = this.createSortBy(columnName, desc)
+      return arr.slice(0).sort(byColumnName)
+    },
+    toggleSort(columnName) {
+      const column = this.headers
+        .find(header => header.name === columnName)
+      if (column.sortable === false) {
+        this.sortDirection = ''
+      } else  if (this.sortingBy !== column.name) {
+        this.sortDirection = 'asc'
+      } else if (this.sortDirection === '') {
+        this.sortDirection = 'asc'
+      } else if (this.sortDirection === 'asc') {
+        this.sortDirection = 'desc'
+      } else {
+        this.sortDirection = ''
+      }
+      this.sortingBy = column.name
     }
   }
 }
