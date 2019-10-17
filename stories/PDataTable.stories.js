37c190a77b860fef8924e6503b5c75566f2e6f26
Romulo G. Rodrigues <yemolai@gmail.com>
2019-10-16 22:41:02 -0300

    no-sort example

--- stories/PDataTable.stories.js
+++ stories/PDataTable.stories.js
@@ -25,3 +25,24 @@ export const basicUsage = () =>
       }
     }
   })
+
+export const disabledSort = () =>
+  Render({
+    components: { PDataTable },
+    template: `<PDataTable no-sort :headers="headers" :items="items" />`,
+    data: function () {
+      return {
+        headers: [
+          { label: 'ColA', name: 'a' },
+          { label: 'ColB', name: 'b' }
+        ],
+        items: [
+          { a: 1, b: 2 },
+          { a: 4, b: 3 },
+          { a: 5, b: 8 },
+          { a: 6, b: 9 },
+          { a: 7, b: 0 }
+        ]
+      }
+    }
+  })
